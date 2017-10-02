e0c6508c9fa845dfa5220b642b85baa0b315cbb7
Unknown <jsandrade1998@gmail.com>
2017-10-02 13:06:54 +0200

    dasda

--- app/src/main/java/com/example/jstalin/contador/MainActivity.java
+++ app/src/main/java/com/example/jstalin/contador/MainActivity.java
@@ -16,16 +16,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         texto = (TextView) findViewById(R.id.text);
 
-        texto.setText(String.valuOf(contador));
+        texto.setText(String.valueOf(contador));
 
     }
 
     // duplica el valor del contador y la guarda en la etiqueta texto
     public void duplica (View view){
 
-        contador = contador + 2;
+        contador = contador * 2;
         texto.setText(String.valueOf(contador));
 
+    }
+
+    // guardar el estado de la variable contador
+    @Override
+    protected void onSaveInstanceState(Bundle estado) {
+        super.onSaveInstanceState(estado);
+        estado.putDouble ("posicion", contador);
+    }
+
+    // 1ª forma de recuperar el estado
+    @Override
+    protected void onRestoreInstanceState(Bundle estado) {
+        super.onRestoreInstanceState (estado);
+        contador = estado.getDouble("posicion");
+        texto.setText (String.valueOf (contador));
     }
 
 }
